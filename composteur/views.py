f617573f05ef36f150d8613f726bd5bf96149096
Florian <florian.lescuyer@gmail.com>
2019-02-19 18:12:03 +0100

    add view for the index page

--- composteur/views.py
+++ composteur/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    """ Home page """
+    return render(request, "composteur/index.html")
