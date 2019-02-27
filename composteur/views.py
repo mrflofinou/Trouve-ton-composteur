3dc03ddcf2bd358e5892ec4d2b1691b3efe2037c
Florian <florian.lescuyer@gmail.com>
2019-02-27 16:02:09 +0100

    update view map for display map fromthe query of a user

--- composteur/views.py
+++ composteur/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .libs.google_map_API import GoogleApiRequest
+
 def index(request):
     """ Home page """
     return render(request, "composteur/index.html")
 
 def map(request):
     """ Page to display the results on a google map """
-    return render(request, "composteur/map.html")
+
+    query = request.GET.get("query")
+    google_request = GoogleApiRequest(query)
+    lat, lng = google_request.get_coordinates()
+    context = {
+        "query" : query,
+        "lat" : str(lat),
+        "lng" : str(lng)
+    }
+    return render(request, "composteur/map.html", context)
