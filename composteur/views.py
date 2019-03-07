ff9566c3ee0dc4670b0e3d2ae70bda2895fb8b4f
Florian <florian.lescuyer@gmail.com>
2019-03-07 12:05:36 +0100

    add view for signup and add management of api key in map view

--- composteur/views.py
+++ composteur/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
 
+from .forms import SignUpForm
 from .libs.google_map_API import GoogleApiRequest
+from .libs.api_keys import google_apis
 
 def index(request):
     """ Home page """
@@ -13,9 +16,34 @@ def map(request):
     query = request.GET.get("query")
     google_request = GoogleApiRequest(query)
     lat, lng = google_request.get_coordinates()
+    javascript_api = google_apis["JAVASCRIPT_API"]
     context = {
         "query" : query,
         "lat" : str(lat),
-        "lng" : str(lng)
+        "lng" : str(lng),
+        "js_api" : javascript_api
     }
     return render(request, "composteur/map.html", context)
+
+def signup(request):
+    """ Page to signup """
+
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            
+            # logger.info('New user registered', exc_info=True, extra={
+            #     # Optionally pass a request and we'll grab any information we can
+            #     'request': request,
+            # })
+
+            return redirect("index")
+    else:
+        form = SignUpForm()
+    context = {"form": form}
+    return render(request, "composteur/signup.html", context)
